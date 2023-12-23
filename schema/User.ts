8227d73b0c4b526af567a3f0f6c814b599090794
paradox8599 <paradox8599@outlook.com>
2023-12-23 22:53:04 +1100

    style: rm unused import; mv fly secret lint to end

--- schema/User.ts
+++ schema/User.ts
@@ -1,6 +1,6 @@
 import { list } from "@keystone-6/core";
 import { allowAll } from "@keystone-6/core/access";
-import { text, password, timestamp, select } from "@keystone-6/core/fields";
+import { text, password, select } from "@keystone-6/core/fields";
 
 import { type Lists } from ".keystone/types";
 import { Role, RoleName } from "../src/lib/types/auth";
