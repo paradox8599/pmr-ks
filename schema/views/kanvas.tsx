206bccee2106e116a33142abe73351064edf22e4
paradox8599 <paradox8599@outlook.com>
2024-01-19 16:14:19 +1100

    feat: canvas image display & auto scale on resize

--- schema/views/kanvas.tsx
+++ schema/views/kanvas.tsx
@@ -1,10 +1,77 @@
-import { Stage, Layer, Circle } from "react-konva";
+import { Stage, Layer, Circle, Image as KonvaImage } from "react-konva";
+import React from "react";
+import Konva from "konva";
 
-export default function Kanvas() {
+import { useJson } from "./hooks/useJson";
+
+type FieldValue = {};
+const initialValue: FieldValue[] = [] as const;
+
+function ClickedCircle({ x, y }: { x: number; y: number }) {
+  return <Circle x={x} y={y} radius={10}></Circle>;
+}
+export default function Kanvas({
+  value,
+  onChange,
+}: {
+  value: string;
+  onChange?: (value: string) => void;
+}) {
+  const { data, setData } = useJson<string>({
+    value,
+    onChange,
+    initialValue,
+  });
+
+  const stageRef = React.useRef<Konva.Stage>(null);
+  const divRef = React.useRef<HTMLDivElement>(null);
+
+  const [ratio, setRatio] = React.useState<number>();
+  const [width, setWidth] = React.useState<number>();
+  const height = React.useMemo(() => {
+    return (width ?? 1) * (ratio ?? 1);
+  }, [width, ratio]);
+
+  const image = React.useMemo(() => {
+    const img = new Image();
+    img.src =
+      "https://pub-b55630bf7f0b47a1b7d4cfd51da53a77.r2.dev/images/d5776adc-5c89-4f1e-b41b-2d42a00a212a.png";
+    img.onload = () => {
+      setRatio(img.height / img.width);
+    };
+    return img;
+  }, []);
+
+  React.useEffect(() => {
+    setWidth(divRef.current?.clientWidth);
+    function onResize() {
+      if (divRef.current?.clientWidth !== width) {
+        setWidth(divRef.current?.clientWidth);
+      }
+    }
+    window.addEventListener("resize", onResize);
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, [width]);
+
   return (
-    <Stage width={300} height={300}>
-      <Layer>
-      </Layer>
-    </Stage>
+    <div ref={divRef} style={{ border: "1px solid black" }}>
+      <Stage
+        ref={stageRef}
+        onClick={(e) => {
+          const pos = stageRef.current?.getRelativePointerPosition();
+        }}
+        height={height}
+        width={width}
+      >
+        {/* Background image layer */}
+        <Layer>
+          <KonvaImage image={image} width={width} height={height}></KonvaImage>
+        </Layer>
+        {/* Circle layer */}
+        <Layer></Layer>
+      </Stage>
+    </div>
   );
 }
--- schema/views/observation.tsx
+++ schema/views/observation.tsx
@@ -1,39 +1,27 @@
+import { type FieldProps } from "@keystone-6/core/types";
 import { type controller } from "@keystone-6/core/fields/types/json/views";
 
-import { type FieldProps } from "@keystone-6/core/types";
-import { FieldContainer, FieldLabel } from "@keystone-ui/fields";
 import React from "react";
-
 import dynamic from "next/dynamic";
+import { FieldContainer, FieldLabel } from "@keystone-ui/fields";
 
-import { useJson } from "./hooks/useJson";
 import { globalStyles as styles } from "./global.style";
 
+// dynamic import to avoid SSR
 const Kanvas = dynamic(() => import("./kanvas"), { ssr: false });
-
-type FieldValue = {};
-const initialValue: FieldValue = {} as const;
 
 export const Field = ({
   field,
   value,
   onChange,
 }: FieldProps<typeof controller>) => {
-  const { data, setData } = useJson<FieldValue>({
-    value,
-    onChange,
-    initialValue,
-  });
-
   return (
     <FieldContainer>
       <FieldLabel>{field.label}</FieldLabel>
       <div style={styles.description}>{field.description}</div>
       <div>
-        <p>Canvas</p>
-        <canvas width={300} height={300}></canvas>
         <div>
-          <Kanvas></Kanvas>
+          <Kanvas value={value} onChange={onChange}></Kanvas>
         </div>
       </div>
     </FieldContainer>
